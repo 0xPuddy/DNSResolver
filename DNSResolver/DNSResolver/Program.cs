b9cd0f69f4da6bf9c233a22c7240eac4ef307025
Puddy (r3mad3) <37082884+0xPuddy@users.noreply.github.com>
2023-02-19 21:02:15 -0500

    Add files via upload

---
+++ DNSResolver/DNSResolver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSResolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Main:
+            Console.Clear();
+            Console.Write("Enter a domain name: ");
+            string domain = Console.ReadLine();
+
+            if (Uri.CheckHostName(domain) == UriHostNameType.Unknown)
+            {
+                Console.WriteLine($"Invalid domain name: {domain}");
+                return;
+            }
+
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(domain);
+
+                Console.WriteLine($"DNS information for {domain}:");
+                foreach (IPAddress ipAddress in hostEntry.AddressList)
+                {
+                    Console.WriteLine(ipAddress);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error getting DNS information for {domain}: {e.Message}");
+            }
+            Console.ReadKey();
+            goto Main;
+        }
+    }
+}
